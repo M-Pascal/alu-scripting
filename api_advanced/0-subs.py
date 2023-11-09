4e1cc61a45ec6af425796c12b1df7360c015c653
M-Pascal <p.mugisha@alustudent.com>
2023-11-09 12:43:36 -0800

    subscribers

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,15 +1,18 @@
-!/usr/bin/python3
-""" 0-subs.py """
-import requests
-
+import json
+import urllib.error
+import urllib.parse
+import urllib.request
 
 def number_of_subscribers(subreddit):
-    """ number_of_subscribers """
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers)
-
-    if response.status_code == 200:
-        return response.json()["data"]["subscribers"]
-    else:
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    req_object = urllib.request.Request(url, method="GET")
+    req_object.add_header('User-Agent', 'OboloScript/1.0')
+    
+    try:
+        with urllib.request.urlopen(req_object) as res_object:
+            res_json = json.JSONDecoder().decode(res_object.read().decode("utf-8"))
+    except urllib.error.HTTPError:
         return 0
+    else:
+        return res_json["data"]["subscribers"]
+
