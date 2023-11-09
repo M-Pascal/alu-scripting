a1b7094e15f43b9c0470deaa98e027dc4c7fe073
M-Pascal <p.mugisha@alustudent.com>
2023-11-09 21:18:55 +0000

    sub

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -2,13 +2,15 @@ import requests
 
 def number_of_subscribers(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Custom User Agent'}  # Set a custom User-Agent header to prevent Too Many Requests errors
-    response = requests.get(url, headers=headers)
+    headers = {'User-Agent': 'Pascal'}
     
-    if response.status_code == 200:
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()  # Raises an HTTPError for bad responses (4xx and 5xx status codes)
         data = response.json()
         subscribers = data['data']['subscribers']
         return subscribers
-    else:
+    except requests.exceptions.HTTPError as err:
+        print(f"Error: {err}")
         return 0
 
