c6b10b1415b7c3b14a0df0aee31c4f1b5c886509
M-Pascal <p.mugisha@alustudent.com>
2023-11-09 22:10:09 +0000

    api_advanced

--- api_advanced/10-top_ten.py
+++ api_advanced/10-top_ten.py
@@ -1,41 +1,20 @@
 #!/usr/bin/python3
-"""
-This module contains the top_ten function that queries the Reddit API
-and prints the titles of the top 10 hot posts of a subreddit.
-"""
-
-
+"""" Top Ten Limit"""
 import requests
 
 
 def top_ten(subreddit):
-    """
-    Prints the titles of the first 10 hot posts of a subreddit.
+    """"top ten"""
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10" \
+        .format(subreddit)
 
-    Args:
-        subreddit (str): The subreddit to query.
+    res = requests.get(url,
+                       headers={
+                           'User-Agent': 'Mozilla/5.0'})
 
-    Prints:
-        The title of each of the first 10 hot posts listed for the given subreddit.
-        Prints None if the subreddit is invalid.
-    """
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {'User-Agent': 'python3:top-ten:v1.0 (by /u/yourusername)'}
-
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        posts = response.json().get('data', {}).get('children', [])
-        for post in posts:
-            print(post['data']['title'])
-    else:
+    if res.status_code != 200:
         print(None)
-
-
-if __name__ == "__main__":
-    import sys
-
-    if len(sys.argv) < 2:
-        print("Usage: ./1-top_ten.py <subreddit>")
-        sys.exit(1)
-
-    top_ten(sys.argv[1])
+    else:
+        json_response = res.json()
+        posts = json_response.get('data').get('children')
+        [print(post.get('data').get('title')) for post in posts]
